3f6a116767a2fcbe59421dc9195a0c0de33e9e17
GlindeAL <2512719869@qq.com>
2022-02-07 15:19:23 +0800

    修改注释

--- glinde-blog-springboot/src/main/java/me/glinde/blog/config/CorsConfig.java
+++ glinde-blog-springboot/src/main/java/me/glinde/blog/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig {
     private CorsConfiguration buildConfig() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setMaxAge(3600L); // 表明在3600秒内，不需要再发送预检验请求
-        corsConfiguration.addAllowedOrigin("http://localhost"); // 允许https://glinde.me域名使用
+        corsConfiguration.addAllowedOrigin("http://localhost"); // 允许http://localhost使用
         corsConfiguration.addAllowedHeader("*"); // 允许任何头
         corsConfiguration.addAllowedMethod("*"); // 允许任何方法（post、get等）
         corsConfiguration.setAllowCredentials(true);
